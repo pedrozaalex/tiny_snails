c2ebeb9761a42a751204158036af822bfcacf9b3
Alexandre Pedroza <pedrozaalexandre@gmail.com>
2021-12-12 17:21:53 -0300

    🚚 route: url -> snails

--- api/index.ts
+++ api/index.ts
@@ -3,14 +3,14 @@ import morgan from 'morgan';
 
 // routers
 import hello from './hello';
-import url from './url';
+import snails from './snails';
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/hello', hello);
-app.use('/url', url);
+app.use('/snails', snails);
 
 export default {
   path: '/api',
