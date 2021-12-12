31e791a5b2193190189ce1e170057cd9c72994d1
Alexandre Pedroza <pedrozaalexandre@gmail.com>
2021-12-12 05:22:36 -0300

    🐛 missing .url access

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -5,15 +5,29 @@
 <script>
 import axios from 'axios';
 export default {
-  async asyncData({ params: { slug }, redirect, $config }) {
+  async fetch({ params: { slug }, redirect, $config }) {
     try {
       const { data: url } = await axios.get(
         `${$config.baseURL}/api/url/${slug}`
       );
-      return redirect(url);
+      return redirect(url.url);
     } catch (error) {
       return redirect('/404.html');
     }
-  }
+  },
+  fetchOnServer: false
+  // async mounted({ params: { slug }, redirect, $config }) {
+  //   if (!process.server) {
+  //     console.log('slug', slug);
+  //     try {
+  //       const { data: url } = await axios.get(
+  //         `${$config.baseURL}/api/url/${slug}`
+  //       );
+  //       return redirect(url);
+  //     } catch (error) {
+  //       return redirect('/404.html');
+  //     }
+  //   }
+  // }
 };
 </script>
