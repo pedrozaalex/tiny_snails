3cb2b9a683af6fedf5967efa0bc9e347ad528beb
Alexandre Pedroza <pedrozaalexandre@gmail.com>
2021-12-12 21:29:54 -0300

    ✨ snail display

---
+++ components/SnailDisplay.vue
@@ -0,0 +1,72 @@
+<template>
+  <CFlex direction="column" align="center">
+    <CHeading>Success!</CHeading>
+
+    <CText> your shortened url: </CText>
+
+    <CFlex
+      text-align="center"
+      rounded="md"
+      border-color="indigo.200"
+      border-width="md"
+      border-style="solid"
+      p="2"
+      px="4"
+      align-items="baseline"
+      w="fit-content"
+    >
+      <CText mr="2"> {{ $config.baseURL }}/{{ snail.alias }} </CText>
+      <CButton variant-color="indigo" p="1" @click="copyUrl"> 📋 </CButton>
+    </CFlex>
+  </CFlex>
+</template>
+
+<script>
+import { CText, CHeading, CFlex } from '@chakra-ui/vue';
+
+export default {
+  components: { CText, CHeading, CFlex },
+  props: {
+    snail: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    copyUrl() {
+      const textToCopy = `${this.$config.baseURL}/${this.snail.alias}`;
+      if (navigator.clipboard) {
+        navigator.clipboard
+          .writeText(textToCopy)
+          .then(function () {
+            alert('Url copied to clipboard');
+          })
+          .catch(function () {
+            alert('err');
+          });
+      } else {
+        const el = document.createElement('textarea');
+        el.value = textToCopy;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        const selected =
+          document.getSelection().rangeCount > 0
+            ? document.getSelection().getRangeAt(0)
+            : false;
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        if (selected) {
+          document.getSelection().removeAllRanges();
+          document.getSelection().addRange(selected);
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
