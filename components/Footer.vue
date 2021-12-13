3585841429f27e2065fc94310e851e238761c5dd
Alexandre Pedroza <pedrozaalexandre@gmail.com>
2021-12-12 21:47:15 -0300

    💄 styling footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,15 +1,32 @@
 <template>
   <footer>
     <CBox pos="absolute" bottom="0" w="100%" p="2">
-      <CText text-align="center"> Made with 💜 by Alexandre Pedroza </CText>
+      <CText text-align="center">
+        Made with 💜 by
+        <CLink
+          class="nameLink"
+          href="https://github.com/pedrozaalex"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Alexandre Pedroza</CLink
+        >
+      </CText>
     </CBox>
   </footer>
 </template>
 
 <script>
-import { CBox, CStack, CText } from '@chakra-ui/vue';
-export default { CBox, CStack, CText };
+import { CBox, CStack, CText, CLink } from '@chakra-ui/vue';
+export default { CBox, CStack, CText, CLink };
 </script>
 
 <style>
+.nameLink {
+  text-decoration: underline;
+}
+.nameLink:hover {
+  text-decoration: none;
+  background-color: #7425f3;
+}
 </style>
