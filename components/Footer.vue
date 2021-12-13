a4d6a46d0a1b621f8a5f41ecb7dbe94225b529ea
Alexandre Pedroza <pedrozaalexandre@gmail.com>
2021-12-12 21:29:34 -0300

    ✨ create footer

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <CBox pos="absolute" bottom="0" w="100%" p="2">
+      <CText text-align="center"> Made with 💜 by Alexandre Pedroza </CText>
+    </CBox>
+  </footer>
+</template>
+
+<script>
+import { CBox, CStack, CText } from '@chakra-ui/vue';
+export default { CBox, CStack, CText };
+</script>
+
+<style>
+</style>
