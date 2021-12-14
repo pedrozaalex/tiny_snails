1c5fce9b19592f1b56996adbb8bd40690042aa23
Alexandre Pedroza <pedrozaalexandre@gmail.com>
2021-12-14 19:08:04 -0300

    💫 main page transitions

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container">
-    <snail-creation
-      v-if="!requestData"
-      @request-sucess="requestData = $event"
-    />
-    <CBox v-else>
-      <snail-display :snail="requestData" @reset="requestData = null" />
-    </CBox>
+    <transition name="slide-fade" mode="out-in">
+      <!-- if no snail data present, show input fields -->
+      <snail-creation v-if="!snailData" @snail-created="snailData = $event" />
+
+      <!-- else, display the snail data -->
+      <snail-display v-else :snail="snailData" @reset="snailData = null" />
+    </transition>
   </div>
 </template>
 
@@ -15,8 +15,25 @@ export default {
   name: 'App',
   data() {
     return {
-      requestData: null
+      snailData: null
     };
   }
 };
 </script>
+
+<style scoped>
+.container {
+  overflow: hidden;
+}
+.slide-fade-enter-active {
+  transition: all 0.1s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.1s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(100px);
+  opacity: 0;
+}
+</style>
