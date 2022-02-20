a8ef86a31cf51c34cf388b6282936ded2dd86ed1
Alexandre Pedroza <pedrozaalexandre@gmail.com>
2022-02-19 21:32:47 -0300

    :children_crossing: Improve redirect page

--- pages/s/_slug.vue
+++ pages/s/_slug.vue
@@ -4,14 +4,18 @@
 
 <script>
 export default {
-  async fetch({ params: { slug }, redirect, $axios }) {
+  async fetch({params: {slug}, redirect, $axios}) {
+    let url;
     try {
-      const { data } = await $axios.get(`/api/snails/${slug}`);
-      return redirect(data.url);
+      url = (await $axios.get(`/api/snails/${slug}`)).data.url;
+      console.log("fetched data", url);
     } catch (error) {
       console.error('error when fetching original url: ', error);
+      console.log(error);
       return redirect('/404');
     }
+
+    return redirect(url);
   },
   fetchOnServer: false
 };
