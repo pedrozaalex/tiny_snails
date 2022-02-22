030322e8f716f2701b0162b2d6e71514e47e71f7
Alexandre Pedroza <pedrozaalexandre@gmail.com>
2022-02-22 17:59:42 -0300

    :lipstick: one line footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,27 +1,23 @@
 <template>
-  <footer>
-    <CStack as="footer" spacing={4} mt={12} textAlign="center">
-      <CText fontSize="sm">
-        made with
-        <CImage src="purple-heart.png" d="inline-block" size="4"/>
-        by
-        <CLink
-          class="nameLink"
-          href="https://github.com/pedrozaalex"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Alexandre Pedroza
-        </CLink
-        >
-      </CText>
-      <CStack mt={4} direction="row" spacing="12px" justify="center">
-        <CLink display="inline-block" href="https://github.com/pedrozaalex/tiny_snails/" aria-label="GitHub" isExternal>
-          <CIcon name="github" size="24px" color="#ac8fd7" />
-        </CLink>
-      </CStack>
-    </CStack>
-  </footer>
+  <CStack as="footer" spacing={4} mt={12} justifyContent="center" flexDirection="row">
+    <CText fontSize="sm">
+      made with
+      <CImage src="purple-heart.png" d="inline-block" size="4"/>
+      by
+      <CLink
+        class="nameLink"
+        href="https://github.com/pedrozaalex"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Alexandre Pedroza
+      </CLink
+      >
+    </CText>
+    <CLink display="inline-block" href="https://github.com/pedrozaalex/tiny_snails/" aria-label="GitHub" isExternal>
+      <CIcon name="github" size="24px" color="#ac8fd7"/>
+    </CLink>
+  </CStack>
 </template>
 
 <style>
